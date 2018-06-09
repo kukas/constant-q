8f04e643eea73711afc947826f3bc161f137b8ff
Jirka Balhar <balhar.j@gmail.com>
2018-06-09 16:37:18 +0200

    fix memoize + add help message

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# constant-q
+# Constant Q transform
+Naive [Constant Q transform](http://academics.wellesley.edu/Physics/brown/pubs/cq1stPaper.pdf) implementation in Haskell. CQT is more suitable for melodic input data than discrete Fourier transform because the transform resolution in lower frequency bands is bigger - in other words the bin frequency/resolution ratio remains constant.
+
+## Usage
+
+```
+Usage: constant-q-exe [OPTIONS]... -i INPUTFILE -o OUTPUTFILE
+Process INPUTFILE (in WAV format) and save the Constant Q transform spectrogram to OUTPUTFILE (png image format).
+Options:
+  -min FREQ Set minimum frequency to FREQ (default 55.0)
+  -max FREQ Set maximum frequency to FREQ (default 11000.0)
+  -b NUM    Set the number of frequency bins in one octave (default 48)
+  -p NUM    Set the hop size (default 1024)
+  -q NUM    Set the Q factor (quality of frequency resolution) (default 72)
+  -h    Print a help message and exit
+```
--- app/Main.hs
+++ app/Main.hs
@@ -15,46 +15,26 @@ drawMatrix matrix = makeImageR RPU (width, height) (\(x, y) -> PixelY $ (matrix 
           height = length matrix
           max = maximum $ map maximum matrix
 
+
+-- indexed map
+mapi :: (Int -> a -> b) -> [a] -> [b]
+mapi f list = zipWith f [0..] list
+
+slidingWindow :: Int -> [Complex Double] -> [[Complex Double]]
+slidingWindow n [] = []
+slidingWindow n samples = [samples++(repeat $ 0:+0)] ++ (slidingWindow n $ drop n $ samples)
+
+-- helper functions for constantq computations
 hannWindow :: Int -> Int -> Double
 hannWindow n m = w*w
     where w = sin (pi * m' / (n'-1))
           m' = fromIntegral m
           n' = fromIntegral n
 
-exp' :: Double -> Int -> Int -> Complex Double
-exp' q k n = cis $ -2 * pi * q * (fromIntegral k) / (fromIntegral n)
-
--- precompexp :: Int -> Int -> (Int -> Int -> Complex Double)
--- precompexp q nmax = trace ("nmax"++show nmax) ([([exp' q k n | k <- [0..n]] !!) | n <- [0..nmax]] !!)
---     -- putStrLn $ "lol"++(show q)++(show nmax)
-
--- indexed map
-mapi :: (Int -> a -> b) -> [a] -> [b]
-mapi f list = zipWith f [0..] list
+cq_exp :: Double -> Int -> Int -> Complex Double
+cq_exp q k n = cis $ -2 * pi * q * (fromIntegral k) / (fromIntegral n)
 
 -- http://academics.wellesley.edu/Physics/brown/pubs/cq1stPaper.pdf
--- constantq :: Int -> Arguments -> [Complex Double] -> [[Double]]
--- constantq _ _ [] = []
--- constantq sr settings xs = [row] ++ (constantq sr settings (drop _hopSize xs))
---     where padxs = xs ++ (repeat (0:+0))
---           row = [magnitude $ 1/((fromIntegral n):+0) * sum (processSamples k n) | k <- [0..maxK], let n = nn k]
---           processSamples k n = zipWith (processSample k n) [0..(n-1)] padxs
---           processSample k n = (\i x -> (hannWindow n i :+ 0)*x*((exp' _qFactor i n)))
-
---           nn k = ceiling $ (fromIntegral sr)*(fromIntegral _qFactor)/(ff k)
---           ff k = 2 ** ((fromIntegral k)/_octaveBins) * _minFreq
---           maxK = ceiling $ log(_maxFreq/_minFreq)/log(2)*_octaveBins
---           precompexp' = precompexp _qFactor (nn 0)
---           _minFreq = minFreq settings
---           _maxFreq = maxFreq settings
---           _qFactor = qFactor settings
---           _octaveBins = fromIntegral $ octaveBins settings
---           _hopSize = hopSize settings
-
-slidingWindow :: Int -> [Complex Double] -> [[Complex Double]]
-slidingWindow n [] = []
-slidingWindow n samples = [samples++(repeat $ 0:+0)] ++ (slidingWindow n $ drop n $ samples)
-
 constantq :: Arguments -> [Complex Double] -> [[Double]]
 constantq settings allsamples = map constq (slidingWindow _hopSize allsamples)
     where
@@ -74,16 +54,17 @@ constantq settings allsamples = map constq (slidingWindow _hopSize allsamples)
         maxBin = ceiling $ log(_maxFreq/_minFreq)/log(2)*_octaveBins
         transformFactor :: Int -> Int -> Complex Double
         transformFactor bin i = x
-            where x = ((hannWindow bwl i) :+ 0)*((exp' _qFactor i bwl))
+            where x = ((hannWindow bwl i) :+ 0)*((cq_exp _qFactor i bwl))
                   bwl = binWindowLength bin
         -- memoized transformFactor using an array
         transformFactorMemoized :: Int -> Int -> Complex Double
         transformFactorMemoized bin i = get i bin
           where
             table = array (0, (maxI+1) * (maxBin+1))
-                [ (x + y * (maxI+1), transformFactor x y)
+                [ (x + y * (maxI+1), transformFactor y x)
                 | y <- [0 .. maxBin]
                 , x <- [0 .. maxI]
+                , x < binWindowLength y
                 ]
             get x y = table ! (x + y * (maxI+1))
             maxI = binWindowLength 0
@@ -118,16 +99,23 @@ parseArgs ("-p":x:xs) args = parseArgs xs (args {hopSize = (read x :: Int)})
 parseArgs ("-q":x:xs) args = parseArgs xs (args {qFactor = (read x :: Int)})
 parseArgs ("-h":xs) args = parseArgs xs (args {help = True})
 
-printHelpIfNeeded Arguments {help = True} = printHelp
-printHelpIfNeeded Arguments {inputFilename = ""} = printHelp
-printHelpIfNeeded Arguments {outputFilename = ""} = printHelp
-printHelpIfNeeded _ = return ()
+printHelpIfNeeded (Arguments {help = True}) defaults = printHelp defaults
+printHelpIfNeeded Arguments {inputFilename = ""} defaults = printHelp defaults
+printHelpIfNeeded Arguments {outputFilename = ""} defaults = printHelp defaults
+printHelpIfNeeded _ _ = return ()
 
-printHelp = do
-    putStrLn $ "Hello, World!"
+printHelp defaults = do
+    name <- getProgName
+    putStrLn $ "Usage: " ++ name ++ " [OPTIONS]... -i INPUTFILE -o OUTPUTFILE"
+    putStrLn $ "Process INPUTFILE (in WAV format) and save the Constant Q transform spectrogram to OUTPUTFILE (png image format)."
+    putStrLn $ "Options:"
+    putStrLn $ "  -min FREQ\tSet minimum frequency to FREQ (default "++(show $ minFreq defaults)++")"
+    putStrLn $ "  -max FREQ\tSet maximum frequency to FREQ (default "++(show $ maxFreq defaults)++")"
+    putStrLn $ "  -b NUM\tSet the number of frequency bins in one octave (default "++(show $ octaveBins defaults)++")"
+    putStrLn $ "  -p NUM\tSet the hop size (default "++(show $ hopSize defaults)++")"
+    putStrLn $ "  -q NUM\tSet the Q factor (quality of frequency resolution) (default "++(show $ qFactor defaults)++")"
+    putStrLn $ "  -h\tPrint a help message and exit"
     exitSuccess
-
-
 
 main :: IO ()
 main = do
@@ -142,7 +130,7 @@ main = do
                                 , hopSize = 1024
                                 , help = False}
     let parsedArgs = parseArgs args defaultArgs
-    printHelpIfNeeded parsedArgs
+    printHelpIfNeeded parsedArgs defaultArgs
     -- mapM_ print $ slowdft [1,1,1,1,0,0,0,0]
     wave <- getWAVEFile (inputFilename parsedArgs)
     let channels = waveNumChannels $ waveHeader wave
